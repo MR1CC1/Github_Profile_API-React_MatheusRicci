cfdab75126b5d4940b16abc9c00e117e66e403ac
Matheus Ricci <57235913+MR1CC1@users.noreply.github.com>
2023-05-22 14:44:49 -0300

    up

--- src/App.js
+++ src/App.js
@@ -5,7 +5,12 @@ import { useEffect, useState } from "react";
 function App() {
   const [dados, setDados] = useState([]);
 
-  const url = "https://api.github.com/users/MR1CC1";
+
+  const inputUrl = "MR1CC1"
+
+  const url = `https://api.github.com/users/${inputUrl}`;
+
+  
 
   useEffect(() => {
     const fetchData = async () => {
@@ -19,31 +24,38 @@ function App() {
   const infos = [{ name: "Login", description: dados.login }];
 
   return (
-    <div className="bg-white">
-      <div className="mx-auto grid max-w-2xl grid-cols-1 items-center gap-x-8 gap-y-16 px-4 py-24 sm:px-6 sm:py-32 lg:max-w-7xl lg:grid-cols-2 lg:px-8">
+    <div className="bg-dark">
+
+      
+
+      <div className="mx-auto grid max-w-2xl grid-cols-2 items-center gap-x-8 gap-y-16 px-4 py-24 sm:px-6 sm:py-32 lg:max-w-7xl lg:grid-cols-3 lg:px-8">
+      
         <div>
-          <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
+          <h2 className="text-3xl font-bold tracking-tight sm:text-4xl">
             GitHub Infos
           </h2>
-          <p className="mt-4 text-gray-500">XXX</p>
+          <p className="mt-4">XXX</p>
 
           <dl className="mt-16 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 sm:gap-y-16 lg:gap-x-8">
             {infos.map((info) => (
               <div key={info.name} className="border-t border-gray-200 pt-4">
-                <dt className="font-medium text-gray-900">{info.name}</dt>
-                <dd className="mt-2 text-sm text-gray-500">
+                <dt className="font-medium">{info.name}</dt>
+                <dd className="mt-2 text-sm">
                   {info.description}
                 </dd>
               </div>
             ))}
           </dl>
         </div>
-        <div className="grid grid-cols-2 grid-rows-1 gap-1 sm:gap-2 lg:gap-2">
+        <div className="avatar">
           <img
             src={dados.avatar_url}
             alt=""
             className="rounded-lg"
           />
+          <input type="text" onChange={() => {
+            console.log(url)
+          }} name="price" id="linkGit"  class="block mt-1 w-full rounded-md border-0 py-1.5 pl-7 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" placeholder="Link do Perfil do GitHub"/>
         </div>
       </div>
     </div>
